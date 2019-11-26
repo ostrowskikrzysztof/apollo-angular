b1cc656428d6eae894e065ef9e2142ee7cd21e1c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-11-26 21:46:01 +0100

    bump

--- packages/apollo-angular/schematics/install/index.ts
+++ packages/apollo-angular/schematics/install/index.ts
@@ -30,7 +30,7 @@ export default function install(options: Schema): Rule {
 }
 
 export const dependenciesMap: Record<string, string> = {
-  '@apollo/client': '^3.0.0-beta.12',
+  '@apollo/client': '^3.0.0-beta.14',
   'apollo-angular': '^2.0.0-alpha.3',
   'apollo-angular-link-http': '^2.0.0-alpha.2',
   graphql: '^14.5.0',
